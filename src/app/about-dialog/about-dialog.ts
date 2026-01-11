20984630298dafd9b7c8c64298b61343fec9e797
Scott Janssens <scottjanssensdev@gmail.com>
2026-01-10 18:39:48 -0600

    Added about dialog content

--- src/app/about-dialog/about-dialog.ts
+++ src/app/about-dialog/about-dialog.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'ory-about-dialog',
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule],
   templateUrl: './about-dialog.html',
   styleUrl: './about-dialog.css',
 })
 export class AboutDialog {
-
+  angularVersion = '21.0';
+  githubUrl = 'https://github.com/scott-janssens/orreryNg';
+  aaJsUrl = 'https://github.com/astronexus/aa-js';
+  wikipediaUrl = 'https://en.wikipedia.org/wiki/Orrery';
+  angularUrl = 'https://angular.dev';
+  threeJsUrl = 'https://threejs.org';
+  angularMaterialUrl = 'https://material.angular.io';
 }
