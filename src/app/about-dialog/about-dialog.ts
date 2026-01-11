40cd334e11fe06d7c46a60c5b2cbb19edbda5727
Scott Janssens <scottjanssensdev@gmail.com>
2026-01-10 18:45:18 -0600

    Tweaked about info

--- src/app/about-dialog/about-dialog.ts
+++ src/app/about-dialog/about-dialog.ts
@@ -8,12 +8,4 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './about-dialog.html',
   styleUrl: './about-dialog.css',
 })
-export class AboutDialog {
-  angularVersion = '21.0';
-  githubUrl = 'https://github.com/scott-janssens/orreryNg';
-  aaJsUrl = 'https://github.com/astronexus/aa-js';
-  wikipediaUrl = 'https://en.wikipedia.org/wiki/Orrery';
-  angularUrl = 'https://angular.dev';
-  threeJsUrl = 'https://threejs.org';
-  angularMaterialUrl = 'https://material.angular.io';
-}
+export class AboutDialog { }
